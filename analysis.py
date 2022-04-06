6338164490b5ac5437cb0d1aa8d4add543625a06
nhinojos <noah.hinojos@gmail.com>
2022-04-06 15:48:12 -0700

    Updated for new Dataframe Setup.

--- analysis.py
+++ analysis.py
@@ -1,9 +1,12 @@
 # Does the physics calulations
 import pandas as pd
-import math
+import numpy as np
 
 # Reads the .csv
 with open("data\\csv\\ee_mll175_electrons.csv") as file:
     df_175=pd.read_csv(file,index_col=0)
-pt=df_175.loc[:,'Momenta']
-phi=df_175.loc[:,"Phi"]
+print(df_175)
+pt_1=df_175.loc[:,"Momenta 1"]
+phi_1=df_175.loc[:,"Phi 1"]
+px_1=pt_1*np.cos(phi_1)
+print(px_1)
